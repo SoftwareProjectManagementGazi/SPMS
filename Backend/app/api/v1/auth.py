fc55b7831f123f6ec4469eb6905f124d779daf9b
Yusuf Emre Bayrakcı <ybayrakci04@gmail.com>
2026-01-08 00:04:28 +0300

    Add get task endpoint and get current user endpoint

--- Backend/app/api/v1/auth.py
+++ Backend/app/api/v1/auth.py
@@ -2,12 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.application.dtos.auth_dtos import UserRegisterDTO, UserLoginDTO, TokenDTO, UserResponseDTO
 from app.application.use_cases.register_user import RegisterUserUseCase
 from app.application.use_cases.login_user import LoginUserUseCase
-from app.api.dependencies import get_user_repo, get_security_service
+from app.api.dependencies import get_user_repo, get_security_service, get_current_user
 from app.domain.repositories.user_repository import IUserRepository
 from app.application.ports.security_port import ISecurityService
+from app.domain.entities.user import User
 from app.domain.exceptions import UserAlreadyExistsError, InvalidCredentialsError
 
 router = APIRouter()
+
+@router.get("/me", response_model=UserResponseDTO)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 @router.post("/register", response_model=UserResponseDTO, status_code=status.HTTP_201_CREATED)
 async def register(
